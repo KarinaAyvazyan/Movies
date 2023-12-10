4aff74d188e8cdb835f1008bd295121e44c9249d
Karina Ayvazyan <ayvazyankarina1@gmail.com>
2023-12-10 11:45:57 +0400

    new  changes

--- Movies/ViewModels/Users/UserViewModel.cs
+++ Movies/ViewModels/Users/UserViewModel.cs
@@ -7,6 +7,7 @@ namespace Movies.ViewModels.Users
     {
        
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
       
